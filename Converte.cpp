766c24c3cd54cdbf7c173abf04051ae64590d00d
Gabriel_Schoepf <73801532+Gabriel-Schoepf@users.noreply.github.com>
2021-11-03 21:12:16 -0300

    Update Converte.cpp

--- Converte.cpp
+++ Converte.cpp
@@ -3,7 +3,7 @@ Implemente um sistema, que o usuario digite
 um valor em metros (m) e conerta 
 para centimetros (cm)
 -- Digite o valor em (m)
---O valor convertido de x(m) é y(cm)
+--O valor convertido de x(m) Ã© y(cm)
 */
 
 #include <stdio.h>
@@ -12,10 +12,10 @@ int main()
 
 {
 	float m, cm; // Metros (m); Centimetros (cm)
-		printf("Bem vindo ao conversor online.\n Digite o valor a ser convertido em metros (m) \n");
-	        scanf("%f",&m); 
-		cm = (m * 100);
-		printf("\n%.0f metros sao %.0f Centimetros",m ,cm);
+	  printf("Bem vindo ao conversor online.\n Digite o valor a ser convertido em metros (m) \n");
+	    scanf("%f",&m); 
+	cm = (m * 100);
+	  printf("\n%.0f metros sao %.0f Centimetros",m ,cm);
 
 	 return 0;
 }
